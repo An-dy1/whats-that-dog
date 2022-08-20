4a40fefab42974da75707081b519f74c3f80d4be
Andy <andrea_sue2@icloud.com>
2022-08-20 16:58:34 -0500

    get guest by id endpoint created

--- api/index.js
+++ api/index.js
@@ -1,22 +1,30 @@
-import data from './resources/guests.json';
 const express = require('express');
 const cors = require('cors');
+const data = require('./resources/guests.json');
 
-const PORT = process.env.PORT || 5005;
+const PORT = process.env.PORT || 5001;
 
 let app = express();
 app.use(cors());
-
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+app.use(express.json());
 
 app.get('/guests', async (req, res) => {
   res.send(data).status(200);
 });
 
 app.get('/guests/:id', async (req, res) => {
-  res.send({ data: data.id === req.params.id }).status(200);
+  const id = req.params.id;
+  let foundGuest;
+  for (let i = 0; i < data.data.length; i++) {
+    if (data.data[i].id.toString() === id) {
+      foundGuest = data.data[i];
+    }
+  }
+  if (foundGuest) {
+    res.send(foundGuest).status(200);
+  } else {
+    res.send({}).status(404);
+  }
 });
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
