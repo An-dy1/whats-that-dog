a9ed6d55aca687cc55b1a74b5a4c0087b52032b6
Andy <andrea_sue2@icloud.com>
2022-10-02 20:53:25 -0500

    working on expanding the card component

--- src/components/user/ExpandableCard.js
+++ src/components/user/ExpandableCard.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpandableCard.css';
 import userIcon from '../resources/user.png';
 
 export default function ExpandableCard() {
+  const [cardIsExpanded, setCardIsExpanded] = useState(false);
+
+  function handleExpandClick(event) {
+    setCardIsExpanded(!cardIsExpanded);
+  }
+
   return (
-    <div className='expandable-card'>
-      <div role='button' class='expand-card-button'>
-        <div className='icon'>
-          <img src={userIcon} alt='user icon' width='80' height='80' />
-        </div>
-        <div className='card-content'>
-          <div className='title'>A title</div>
-          <div className='sub-title'>
-            A subtitle with some extra words in it.
+    <div style={{ height: cardIsExpanded ? '12vh' : '36vh' }}>
+      <div className='expandable-card'>
+        <div role='button' class='expand-card-button'>
+          <div className='card-icon'>
+            <img src={userIcon} alt='user icon' width='80' height='80' />
+          </div>
+          <div className='card-content'>
+            <div className='title'>View Photos</div>
+            <div className='subtitle'>A collection of photos of Wesley.</div>
+          </div>
+          <div className='expand'>
+            <button onClick={handleExpandClick}>Expand me</button>
           </div>
         </div>
+      </div>
+      <div
+        className='expanded-content'
+        style={{ visibility: cardIsExpanded ? 'visible' : 'hidden' }}
+      >
+        <p>Hello</p>
       </div>
     </div>
   );
